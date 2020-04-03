ed3b3c9e211711741e5ec0bcdd40069b0bdca750
alyssaschilke <arschilke@loyola.edu>
2020-04-03 17:51:10 -0400

    Friday so far

--- web pages/Projects/Barcoding_The_Harbor.php
+++ web pages/Projects/Barcoding_The_Harbor.php
@@ -1,10 +1,117 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
+
+<html>
   <head>
-    <meta charset="utf-8">
-    <title>Barcoding The Harbor</title>
+    <!-- set viewport to screen size -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Master Stylesheet -->
+    <link rel="stylesheet" href="../../css/SASS/BUGSS_Styles.css">
+
+    <!-- CDNs for Bootstrap and jquery -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+    <!-- Favicon Settings for Different Devices -->
+    <link rel="apple-touch-icon" sizes="57x57" href="../../media/favicon/apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="../../media/favicon/apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="../../media/favicon/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="../../media/favicon/apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="../../media/favicon/apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="../../media/favicon/apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="../../media/favicon/apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="../../media/favicon/apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="../../media/favicon/apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="192x192" href="../../media/favicon/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="../../media/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="../../media/favicon/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="../../media/favicon/favicon-16x16.png">
+    <link rel="manifest" href="../../media/favicon/manifest.json">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-TileImage" content="../../media/favicon/ms-icon-144x144.png">
+    <meta name="theme-color" content="#ffffff">
+
+    <!--font awesome cdn for social Buttons-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title>Scramble and Synthetic Yeast</title>
+
   </head>
-  <body>
+  <body class="bg-black">
+    <!--TODO: Update Active classes on links if applicable-->
+    <!--NAVBAR-->
+    <nav class="navbar navbar-light navbar-expand-lg bg-white">
+      <a class="navbar-brand" href="#"><img class="logoNav" src="../../media/bugsslogo-square300.png"></a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarColor02">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="#">About</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Membership</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active" href="">Group Projects</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">For Scientists</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">For Community Members</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">For Kids and Parents</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Support Us</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+
+    <ul class="nav nav-fill nav-pills bg-primary mb-3">
+      <li class="nav-item">
+        <a class="nav-link text-white active rounded-0" href="Barcoding_The_Harbor.php">Barcoding the Harbor</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link text-white" href="Scramble_Synthetic_Yeast.html">Scramble and Synthetic Yeast</a>
+      </li>
+    </ul>
+
+
+
+
+
+
+
+
+
+
+    <!--FOOTER Default BG/ACCENT TEXT-->
+    <footer>
+      <!-- Add font awesome icons -->
+      <hr>
+      <div class="row justify-content-around text-center">
+        <div class="col text-secondary">
+          Address: <br>
+          101 North Haven Street,<br>
+          Baltimore MD 21224
+        </div>
+        <div class="col" style="font-size: 3.5rem;">
+          <a href="https://www.facebook.com/BUGSS-Baltimore-Under-Ground-Science-Space-275707269195705/" class="fa fa-facebook text-decoration-none"></a>
+          <a href="https://twitter.com/bugsslab" class="fa fa-twitter text-decoration-none"></a>
+          <a href="https://www.youtube.com/channel/UCr4bqCH50xDCnrWBCUbobBQ" class="fa fa-youtube text-decoration-none"></a>
+          <a href="https://www.instagram.com/bugsslab/" class="fa fa-instagram text-decoration-none"></a>
+        </div>
+        <div class="col text-secondary">
+          Email: <a class="link-light" href="mailto:info@bugssonline.org">info@bugssonline.org</a>
+        </div>
+      </div>
+    </footer>
 
   </body>
-</html>
+
+  </html>
